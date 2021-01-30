2d526eb18b132cd592d7dceb23dd276939a6ede2
volovar <michaelvolovar@gmail.com>
2021-01-30 18:24:09 -0500

    feat(wip): added image endpoint
    Added endpoint to get images

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import logger from "./logger";
 import router from "./routes";
+import { config } from "dotenv"
+
+config()
 
 const app = express();
 const port = process.env.PORT || 9010;
+
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next()
+})
 
 app.use("/", router);
 
