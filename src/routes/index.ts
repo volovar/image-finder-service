a3953484ede7820a74b76f6cc9a13ed5f01ee667
volovar <michaelvolovar@gmail.com>
2021-02-10 20:14:28 -0500

    feat(integrate aws): added aws
    Testing aws s3 integration in node

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,30 +1,56 @@
 import { Request, Response, Router } from "express";
 import axios from "axios";
 import logger from "../logger";
+import S3 from "aws-sdk/clients/s3";
 
 const router = Router();
+const s3 = new S3({ region: "us-west-2" });
+
+const getBucketData = async () => {
+  const params = { Bucket: "smithsonian-open-access" };
+
+  try {
+    s3.listObjectsV2(params, (err, data) => {
+      if (err) throw err;
+
+      return data;
+    });
+  } catch (err) {
+    logger.error(err);
+  }
+};
 
 const getData = async (params: object | URLSearchParams) => {
+  logger.info(process.env);
   try {
-    const response = await axios.get("https://api.si.edu/openaccess/api/v1.0/search", { params })
+    const response = await axios.get(
+      "https://api.si.edu/openaccess/api/v1.0/search",
+      { params }
+    );
 
-    return response.data
+    return response.data;
   } catch (err) {
-    logger.error(err)
+    logger.error(err);
   }
-}
+};
 
 router.get("/images", async (req: Request, res: Response) => {
-  logger.info(process.env.API_KEY)
-  logger.info(req.query)
+  logger.info(process.env.API_KEY);
+  logger.info(req.query);
 
   const data = await getData({
     api_key: process.env.API_KEY,
-    q: req.query.q
-  })
+    q: req.query.q,
+  });
 
-  res.send(data)
-})
+  res.send(data);
+});
+
+router.get("/bucket", async (_, res) => {
+  const data = await getBucketData();
+
+  res.send(data);
+});
 
 router.get("/", (_: Request, res: Response) => {
   res.send("Service is healthy on /");
